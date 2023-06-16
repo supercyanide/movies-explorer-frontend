493bec308321f4afa0f7021a6fbfdae696831154
Lilya Schedrina <13cutepie@gmail.com>
2023-06-16 17:34:12 +0400

    Update ProfileInput.js

--- src/components/ProfileInput/ProfileInput.js
+++ src/components/ProfileInput/ProfileInput.js
@@ -1,6 +1,6 @@
 import './ProfileInput.css';
 
-export default function ProfileInput({ caption, name, placeholder, value, onChange }) {
+export default function ProfileInput({ caption, name, placeholder, value, onChange, isReqired}) {
 
     return (
     <label className='profile-input__label'>
@@ -9,6 +9,7 @@ export default function ProfileInput({ caption, name, placeholder, value, onChan
             placeholder={placeholder}
             name={name}
             className="profile-input__input"
+            required={isReqired}
             value={value}
             onChange={({ target }) => onChange(target.value)} 
         />
