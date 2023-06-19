543b391b92477e5b3580739aae73b0ea2351b07b
Lilya Schedrina <13cutepie@gmail.com>
2023-06-20 00:49:59 +0400

    fix: исправить стили

--- src/components/Page404/Page404.js
+++ src/components/Page404/Page404.js
@@ -4,12 +4,15 @@ import { useNavigate } from 'react-router-dom';
 export default function Page404({ link }) {
     const navigate = useNavigate();
     return (
-        <section className='page404'>
-            <div className='page404__info'>
-                <h2 className="page404__title">404</h2>
-                <p className="page404__subtitle">Страница не найдена</p>
-            </div>
-            <button type="button" onClick={() => navigate(-1)} className="page404__link">Назад</button>
-        </section>
+        <main className='main'>
+            <section className='page404'>
+                <div className='page404__info'>
+                    <h2 className="page404__title">404</h2>
+                    <p className="page404__subtitle">Страница не найдена</p>
+                </div>
+                <button type="button" onClick={() => navigate(-1)} className="page404__link">Назад</button>
+            </section>
+        </main>
+        
     )
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import './Profile.css';
-import ProfileInput from '../ProfileInput/ProfileInput';
+import ProfileInput from './ProfileInput/ProfileInput';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -23,27 +23,30 @@ export default function Profile() {
         setEmail(evt.target.email.value);
     }
     return (
-        <form className="profile" name='profile' onSubmit={handleSubmit}>
-            <h2 className="profile__title">Привет, {name}!</h2>
-            <fieldset className="profile__inputs">
-                <ProfileInput caption="Имя" name='name' placeholder='Введите имя' value={name} onChange={handleNameChange} isRequired={true} isDisabled={isDisabled}/>
-                <ProfileInput caption="E-mail" name='email' placeholder='Введите адрес электронной почты' value={email} onChange={handleEmailChange} isRequired={true} isDisabled={isDisabled} />
-            </fieldset>
-            <fieldset className="profile__buttons">
-                {isSubmitVisible ?
-                    <div className="profile__submit-block ${isSubmitVisible">
-                        <span className="profile__error">При обновлении профиля произошла ошибка.</span>
-                        <button className="profile__submit" onClick={toggleControlsVisibility}>Сохранить</button>
-                    </div>
-                :
-                    <div className="profile__control-block">
-                        <button type="button" className="profile__control" onClick={toggleControlsVisibility}>Редактировать</button>
-                        <Link to='/' className="profile__control profile__control_signout">Выйти из аккаунта</Link>
-                    </div>
-                }
-                
-                
-            </fieldset>
-        </form>
+        <main className='main'>
+            <section className='profile'>
+                <form className="profile-form" name='profile' onSubmit={handleSubmit}>
+                    <h2 className="profile-form__title">Привет, {name}!</h2>
+                    <fieldset className="profile-form__inputs">
+                        <ProfileInput caption="Имя" name='name' placeholder='Введите имя' value={name} onChange={handleNameChange} isRequired={true} isDisabled={isDisabled}/>
+                        <ProfileInput caption="E-mail" name='email' placeholder='Введите адрес электронной почты' value={email} onChange={handleEmailChange} isRequired={true} isDisabled={isDisabled} />
+                    </fieldset>
+                    <fieldset className="profile-form__buttons">
+                        {isSubmitVisible ?
+                            <div className="profile-form__submit-block">
+                                <span className="profile-form__error">При обновлении профиля произошла ошибка.</span>
+                                <button className="profile-form__submit-btn" disabled={true} onClick={toggleControlsVisibility}>Сохранить</button>
+                            </div>
+                        :
+                            <div className="profile-form__control-block">
+                                <button type="button" className="profile-form__control" onClick={toggleControlsVisibility}>Редактировать</button>
+                                <Link to='/' className="profile-form__control profile-form__control_signout">Выйти из аккаунта</Link>
+                            </div>
+                        }
+                    </fieldset>
+                </form>
+            </section>
+        </main>
+        
     );
 }
