3163108eb5661e7d7afef5bb61fca2554dd71b54
Lilya Schedrina <13cutepie@gmail.com>
2023-07-03 20:53:16 +0400

    hotfix

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,5 +1,5 @@
 const MOVIE_URL = 'https://api.nomoreparties.co';
-const mainApi = 'http://localhost:3000';
+const mainApi = 'https://api.movies.supercyanide.nomoredomains.rocks';
 export default mainApi;
 
 export {
