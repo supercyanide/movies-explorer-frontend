b53474e1bae8cef17bfb8e0c788de52d95e2844a
Lilya Schedrina <13cutepie@gmail.com>
2023-07-23 20:52:11 +0400

    hotfix

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://api.movies.supercyanide.nomoredomains.rocks';
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = 'https://movies-api-js2p.onrender.com';
+// const BASE_URL = 'http://localhost:3000';
 
 function checkResponse(res) {
   if (res.ok) {
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,7 +1,7 @@
 // const MOVIE_URL = 'https://api.nomoreparties.co';
 const MOVIE_URL = 'https://search.imdbot.workers.dev';
-// const mainApi = 'https://api.movies.supercyanide.nomoredomains.rocks';
-const mainApi = 'http://localhost:3000';
+const mainApi = 'https://movies-api-js2p.onrender.com';
+// const mainApi = 'http://localhost:3000';
 export default mainApi;
 
 export {
